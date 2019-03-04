f51d18383ba4d770b9d243842af331b759e9fe66
yul <yu.li@tecsys.com>
2019-03-04 08:09:58 -0500

    JUnit5 group assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,47 @@
+//: guru.springframework.sfgpetclinic.model.PersonTest.java
+
+
+package guru.springframework.sfgpetclinic.model;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class PersonTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void groupAssertions() {
+		// Given
+		Person person = new Person(1l, "Joe", "Buck");
+		
+		// When
+		
+		// Then
+		assertAll("Test Props Set", 
+				() -> assertEquals(person.getFirstName(), "Joe"), 
+				() -> assertEquals(person.getLastName(), "Buck"));
+	}
+	
+	@Test
+	void groupAssertionsMsgs() {
+		// Given
+		Person person = new Person(1l, "Joe", "Buck");
+		
+		// When
+		
+		// Then
+		assertAll("Test Props Set", 
+				() -> assertEquals(person.getFirstName(), "Joe2", 
+						() -> "First Name Failed!"), 
+				() -> assertEquals(person.getLastName(), "Bucko", 
+						() -> "Last Name Failed!"));
+	}
+
+}///:~

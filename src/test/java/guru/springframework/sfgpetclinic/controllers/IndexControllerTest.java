5a8ed8f44a0e30c92bc7fe1bcb29daa9cbd92616
yul <yu.li@tecsys.com>
2019-03-05 08:28:58 -0500

    Test Assumptions

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -4,14 +4,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.Duration;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 
 class IndexControllerTest {
@@ -46,6 +48,12 @@ class IndexControllerTest {
 		});
 	}
 	
+	/*
+	 * Note: the executable will be executed in a different thread than
+	 * that of the calling code. 
+	 * Furthermore, execution of the executable will be preemptively aborted 
+	 * if the timeout is exceeded. 
+	 */
 	@Disabled
 	@Test
 	void testTimeoutPreemptively() {
@@ -53,6 +61,22 @@ class IndexControllerTest {
 			Thread.sleep(3000);
 			System.out.println("I got here Preemptively.");
 		});
+	}
+	
+	/*
+	 * Failed assumptions do not result in a test failure; rather, 
+	 * a failed assumption results in a test being aborted.
+	 */
+	@Test
+	void testAssumptionTrueForFailedSituation() {
+		assumeTrue("Ubuntu_16".equalsIgnoreCase(System.getenv("OS")));
+	}
+	
+	@Test
+	void testAssumptionTrueForReal() {
+		// Given
+		String os = "Ubuntu_16";
+		assumeTrue("Ubuntu_16".equalsIgnoreCase(os));
 	}
 
 }///:~

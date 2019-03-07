bd1928058e900ca5b1109cdf019772298f9a525f
yul <yu.li@tecsys.com>
2019-03-07 12:39:16 -0500

    Config Intergration Test Env.

--- src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,48 @@
+//: guru.springframework.sfgpetclinic.services.springdatajpa.PetTypeSDJpaServiceTest.java
+
+
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayName("Intergration Test for PetType - ")
+class PetTypeSDJpaServiceIT {
+
+	@DisplayName("Initialize Enviromment - ")
+	@BeforeAll
+	static void init() {
+		
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindAll() {
+	}
+
+	@Test
+	void testFindById() {
+	}
+
+	@Test
+	void testSave() {
+	}
+
+	@Test
+	void testDelete() {
+	}
+
+	@Test
+	void testDeleteById() {
+	}
+
+}///:~
